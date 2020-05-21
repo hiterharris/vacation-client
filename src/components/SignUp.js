2c03e2b9601dd58034f8b662b6ec8682be93d92f
Hiter Harris <hiterharris@comcast.net>
2020-05-21 18:47:04 -0400

    sign up page adds user to database

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import '../styles/signup.scss';
 
-function SignUp() {
+function SignUp(props) {
+  const [newUser, setNewUser] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3001/api/auth/register', newUser)
+        .then(response => {
+          console.log(response);
+        })
+  }
+
+  const history = useHistory()
+  const routeToSignIn = () => {
+      history.push('/login')
+  }
+
+
   return (
     <div className="SignUp">
-      <h1>SignUp</h1>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='input'>
+            <input
+              onChange={handleChange}
+              id='username'
+              type='text'
+              name='username'
+              placeholder='Username'
+            />
+        </div>
+        
+        <div className='input'>
+            <input
+              onChange={handleChange}
+              id='password'
+              type='text'
+              name='password'
+              placeholder='Password'
+            />
+        </div>
+
+        <div>
+            <button onClick={routeToSignIn} type='submit'>Sign Up</button>
+        </div>
+
+      </form>
     </div>
   );
 }
